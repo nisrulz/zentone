e88cd3b2c20b380c98483b9bb36a21cabbfd8a42
Nishant Srivastava <nisrulz@gmail.com>
2015-08-28 00:19:51 -0700

    (*) Edit text empty state handling

--- app/src/main/java/in/excogitation/library_zentone/MainActivity.java
+++ app/src/main/java/in/excogitation/library_zentone/MainActivity.java
@@ -6,42 +6,47 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import in.excogitation.zentone.library.ZenTone;
 
 
 public class MainActivity extends ActionBarActivity {
 
-	EditText txt;
-	int freq = 5000;
+    EditText txt;
+    int freq = 5000;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-		Button btn1 = (Button) findViewById(R.id.button);
-		Button btn2 = (Button) findViewById(R.id.button2);
-		txt = (EditText) findViewById(R.id.editText);
+        Button btn1 = (Button) findViewById(R.id.button);
+        Button btn2 = (Button) findViewById(R.id.button2);
+        txt = (EditText) findViewById(R.id.editText);
 
-		btn1.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				freq = Integer.parseInt(txt.getText().toString());
-				txt.setText(Integer.toString(freq));
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!txt.getText().toString().equals("")) {
+                    freq = Integer.parseInt(txt.getText().toString());
+                    // Play Tone
+                    ZenTone.getInstance().generate(freq, 1);
+                } else {
+                    Toast.makeText(MainActivity.this, "Please enter a frequency!", Toast.LENGTH_SHORT).show();
+                }
 
-				ZenTone.getInstance().generate(freq,1);
+            }
+        });
 
-			}
-		});
-
-		btn2.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				ZenTone.getInstance().stop();
-			}
-		});
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Stop Tone
+                ZenTone.getInstance().stop();
+            }
+        });
 
 
-	}
+    }
 }
