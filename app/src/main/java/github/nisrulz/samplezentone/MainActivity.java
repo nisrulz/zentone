83eace2e3f6f055e0a8faa12c774c4af6bc509f6
Nishant Srivastava <nisrulz@gmail.com>
2017-02-12 13:58:28 -0800

    code cleanup and refactoring

--- app/src/main/java/github/nisrulz/samplezentone/MainActivity.java
+++ app/src/main/java/github/nisrulz/samplezentone/MainActivity.java
@@ -6,9 +6,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.SeekBar;
-import android.widget.Toast;
-import github.nisrulz.zentone.ToneStoppedListener;
-import github.nisrulz.zentone.ZenTone;
+
+import static github.nisrulz.zentone.ZenTone.AudioToneListener;
+import static github.nisrulz.zentone.ZenTone.getInstance;
 
 /**
  * The type Main activity.
@@ -22,7 +22,8 @@ public class MainActivity extends AppCompatActivity {
   private boolean isPlaying = false;
   private FloatingActionButton myFab;
 
-  @Override protected void onCreate(Bundle savedInstanceState) {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
@@ -43,62 +44,102 @@ public class MainActivity extends AppCompatActivity {
     });
 
     seekBarFreq.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-      @Override public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+      @Override
+      public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
         editTextFreq.setText(String.valueOf(progress));
       }
 
-      @Override public void onStartTrackingTouch(SeekBar seekBar) {
+      @Override
+      public void onStartTrackingTouch(SeekBar seekBar) {
         // Stop Tone
-        ZenTone.getInstance().stop();
+        getInstance().stop();
       }
 
-      @Override public void onStopTrackingTouch(SeekBar seekBar) {
+      @Override
+      public void onStopTrackingTouch(SeekBar seekBar) {
         //Do nothing
       }
     });
 
     seekBarDuration.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-      @Override public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+      @Override
+      public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
         editTextDuration.setText(String.valueOf(progress));
       }
 
-      @Override public void onStartTrackingTouch(SeekBar seekBar) {
+      @Override
+      public void onStartTrackingTouch(SeekBar seekBar) {
         // Stop Tone
-        ZenTone.getInstance().stop();
+        getInstance().stop();
       }
 
-      @Override public void onStopTrackingTouch(SeekBar seekBar) {
-          // Do nothing
+      @Override
+      public void onStopTrackingTouch(SeekBar seekBar) {
+        // Do nothing
       }
     });
   }
 
   private void handleTonePlay() {
-    String freqString = editTextFreq.getText().toString();
-    String durationString = editTextDuration.getText().toString();
-    if (!"".equals(freqString) && !"".equals(durationString)) {
-      if (!isPlaying) {
-        myFab.setImageResource(R.drawable.ic_stop_white_24dp);
-        freq = Integer.parseInt(freqString);
-        duration = Integer.parseInt(durationString);
-        // Play Tone
-        ZenTone.getInstance().generate(freq, duration, 0.01f, new ToneStoppedListener() {
-          @Override public void onToneStopped() {
-            isPlaying = false;
-            myFab.setImageResource(R.drawable.ic_play_arrow_white_24dp);
-          }
-        });
-        isPlaying = true;
-      } else {
-        // Stop Tone
-        ZenTone.getInstance().stop();
-        isPlaying = false;
-        myFab.setImageResource(R.drawable.ic_play_arrow_white_24dp);
-      }
-    } else if ("".equals(freqString)) {
-      Toast.makeText(MainActivity.this, "Please enter a frequency!", Toast.LENGTH_SHORT).show();
-    } else if ("".equals(durationString)) {
-      Toast.makeText(MainActivity.this, "Please enter duration!", Toast.LENGTH_SHORT).show();
+    if (isPlaying) {
+      getInstance().stop();
+    }
+    else {
+      String freqString = editTextFreq.getText().toString();
+      String durationString = editTextDuration.getText().toString();
+      freq = Integer.parseInt(freqString);
+      duration = Integer.parseInt(durationString);
+      // Play Tone
+      getInstance().generate(freq, duration, 0.01f,audioToneListener);
     }
   }
+
+  AudioToneListener audioToneListener = new AudioToneListener() {
+    @Override
+    public void onToneStarted() {
+      isPlaying = true;
+    }
+
+    @Override
+    public void onToneStopped() {
+      isPlaying = false;
+    }
+  };
+
+  //private void handleTonePlay() {
+  //  String freqString = editTextFreq.getText().toString();
+  //  String durationString = editTextDuration.getText().toString();
+  //  if (!"".equals(freqString) && !"".equals(durationString)) {
+  //    if (!isPlaying) {
+  //      freq = Integer.parseInt(freqString);
+  //      duration = Integer.parseInt(durationString);
+  //      // Play Tone
+  //      ZenTone.getInstance().generate(freq, duration, 0.01f, new ZenTone.AudioToneListener() {
+  //        @Override
+  //        public void onToneStarted() {
+  //          isPlaying = true;
+  //          //myFab.setImageResource(R.drawable.ic_stop_white_24dp);
+  //        }
+  //
+  //        @Override
+  //        public void onToneStopped() {
+  //          isPlaying = false;
+  //          //myFab.setImageResource(R.drawable.ic_play_arrow_white_24dp);
+  //        }
+  //      });
+  //    }
+  //    else {
+  //      // Stop Tone
+  //      ZenTone.getInstance().stop();
+  //      isPlaying = false;
+  //      myFab.setImageResource(R.drawable.ic_play_arrow_white_24dp);
+  //    }
+  //  }
+  //  else if ("".equals(freqString)) {
+  //    Toast.makeText(MainActivity.this, "Please enter a frequency!", Toast.LENGTH_SHORT).show();
+  //  }
+  //  else if ("".equals(durationString)) {
+  //    Toast.makeText(MainActivity.this, "Please enter duration!", Toast.LENGTH_SHORT).show();
+  //  }
+  //}
 }
--- zentone/src/main/java/github/nisrulz/zentone/ZenTone.java
+++ zentone/src/main/java/github/nisrulz/zentone/ZenTone.java
@@ -1,63 +1,188 @@
 package github.nisrulz.zentone;
 
-import android.os.Handler;
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
+import android.os.Build;
 
-/**
- * The type Zen tone.
- */
 public class ZenTone {
-  private PlayToneThread playToneThread;
-  private boolean isThreadRunning = false;
-  private final Handler stopThread;
+  private AudioToneListener audioToneListener;
+  private Thread audioPlayingThread = null;
+  private int freqOfTone;
+  private int duration;
+  private float volume = 0f;
+
+  private boolean shouldContinueProcessingAudio;
 
   private static final ZenTone INSTANCE = new ZenTone();
 
   private ZenTone() {
-    stopThread = new Handler();
+
   }
-  /**
-   * Gets instance.
-   *
-   * @return the instance
-   */
+
   public static ZenTone getInstance() {
     return INSTANCE;
   }
 
+  public void generate(int freqOfTone, int duration, float volume,
+      AudioToneListener audioToneListener) {
+    this.audioToneListener = audioToneListener;
+    this.freqOfTone = freqOfTone;
+    this.duration = duration;
+    this.volume = volume;
+    start();
+  }
 
-
-  /**
-   * Generate pure tone
-   *
-   * @param freq the freq
-   * @param duration the duration
-   * @param volumne the volumne
-   * @param toneStoppedListener the tone stopped listener
-   */
-  public void generate(int freq, int duration, float volumne,
-      ToneStoppedListener toneStoppedListener) {
-    if (!isThreadRunning) {
-      stop();
-      playToneThread = new PlayToneThread(freq, duration, volumne, toneStoppedListener);
-      playToneThread.start();
-      isThreadRunning = true;
-      stopThread.postDelayed(new Runnable() {
-        @Override public void run() {
-          stop();
-        }
-      }, duration * 1000);
+  void start() {
+    if (audioPlayingThread == null) {
+      audioPlayingThread = new Thread(audioPlayRunnable);
+      audioPlayingThread.start();
+    }
+    else if (audioPlayingThread.isAlive()) {
+      stopThreadAndProcessing();
+      audioPlayingThread = new Thread(audioPlayRunnable);
+      audioPlayingThread.start();
     }
   }
 
-  /**
-   * Stop.
-   */
-  public void stop() {
-    if (playToneThread != null) {
-      playToneThread.stopTone();
-      playToneThread.interrupt();
-      playToneThread = null;
-      isThreadRunning = false;
+  private void stopThreadAndProcessing() {
+    // Stop audio processing
+    shouldContinueProcessingAudio = false;
+    // interrupt the thread
+    if (audioPlayingThread != null) {
+      audioPlayingThread.interrupt();
+      audioPlayingThread = null;
     }
+  }
+
+  public void stop() {
+    stopThreadAndProcessing();
+    audioToneListener = null;
+  }
+
+  private final Runnable audioPlayRunnable = new Runnable() {
+    @Override
+    public void run() {
+      // Setup thread priority
+      android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_AUDIO);
+
+      int sampleRate = 8000;
+
+      double dnumSamples = (double) duration * sampleRate;
+      dnumSamples = Math.ceil(dnumSamples);
+      int numSamples = (int) dnumSamples;
+      double[] sample = new double[numSamples];
+      byte[] generatedSnd = new byte[2 * numSamples];
+
+      for (int i = 0; i < numSamples; ++i) {      // Fill the sample array
+        sample[i] = Math.sin(freqOfTone * 2 * Math.PI * i / (sampleRate));
+      }
+
+      // convert to 16 bit pcm sound array
+      // assumes the sample buffer is normalized.
+      // convert to 16 bit pcm sound array
+      // assumes the sample buffer is normalised.
+      int idx = 0;
+      int i;
+
+      int ramp = numSamples / 20;  // Amplitude ramp as a percent of sample count
+
+      for (i = 0; i < ramp; ++i) {  // Ramp amplitude up (to avoid clicks)
+        // Ramp up to maximum
+        final short val = (short) (sample[i] * 32767 * i / ramp);
+        // in 16 bit wav PCM, first byte is the low order byte
+        generatedSnd[idx++] = (byte) (val & 0x00ff);
+        generatedSnd[idx++] = (byte) ((val & 0xff00) >>> 8);
+      }
+
+      for (i = ramp; i < numSamples - ramp;
+          ++i) {                        // Max amplitude for most of the samples
+        // scale to maximum amplitude
+        final short val = (short) (sample[i] * 32767);
+        // in 16 bit wav PCM, first byte is the low order byte
+        generatedSnd[idx++] = (byte) (val & 0x00ff);
+        generatedSnd[idx++] = (byte) ((val & 0xff00) >>> 8);
+      }
+
+      for (i = numSamples - ramp; i < numSamples; ++i) { // Ramp amplitude down
+        // Ramp down to zero
+        final short val = (short) (sample[i] * 32767 * (numSamples - i) / ramp);
+        // in 16 bit wav PCM, first byte is the low order byte
+        generatedSnd[idx++] = (byte) (val & 0x00ff);
+        generatedSnd[idx++] = (byte) ((val & 0xff00) >>> 8);
+      }
+
+      int bufferSize = AudioTrack.getMinBufferSize(sampleRate, AudioFormat.CHANNEL_OUT_MONO,
+          AudioFormat.ENCODING_PCM_16BIT);
+      AudioTrack audioTrack =
+          new AudioTrack(AudioManager.STREAM_MUSIC, sampleRate, AudioFormat.CHANNEL_OUT_MONO,
+              AudioFormat.ENCODING_PCM_16BIT, bufferSize, AudioTrack.MODE_STREAM);
+
+
+      if(audioTrack.getState()!=AudioTrack.STATE_INITIALIZED){
+        return;
+      }
+
+      audioTrack.setNotificationMarkerPosition(numSamples);
+      audioTrack.setPlaybackPositionUpdateListener(
+          new AudioTrack.OnPlaybackPositionUpdateListener() {
+            @Override
+            public void onPeriodicNotification(AudioTrack track) {
+              // nothing to do
+            }
+
+            @Override
+            public void onMarkerReached(AudioTrack track) {
+              audioToneListener.onToneStopped();
+            }
+          });
+
+      // Sanity Check for max volume, set after write method to handle issue in android
+      // v 4.0.3
+      float maxVolume = AudioTrack.getMaxVolume();
+
+      if (volume > maxVolume) {
+        volume = maxVolume;
+      }
+      else if (volume < 0) {
+        volume = 0;
+      }
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        audioTrack.setVolume(volume);
+      }
+      else {
+        audioTrack.setStereoVolume(volume, volume);
+      }
+
+      // start playing
+      audioTrack.play();
+      audioToneListener.onToneStarted();
+      shouldContinueProcessingAudio = true;
+
+      while (shouldContinueProcessingAudio) {
+        audioTrack.write(generatedSnd, 0, generatedSnd.length);
+      }
+
+      // stop recording and release the microphone
+      try {
+        if (audioTrack != null) {
+          audioTrack.stop();
+        }
+      } catch (Exception e) {
+        e.printStackTrace();
+      } finally {
+        if (audioTrack != null) {
+          audioTrack.release();
+          audioTrack = null;
+        }
+      }
+    }
+  };
+
+  public interface AudioToneListener {
+
+    void onToneStarted();
+
+    void onToneStopped();
   }
 }
