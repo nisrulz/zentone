b311de042bf33dd8ba80ca76558630487f334586
Nishant Srivastava <nisrulz@users.noreply.github.com>
2022-07-13 01:11:17 +0200

    Make upload to Sonatype less flaky (#28)
    * Make upload to Sonatype less flaky
    
    * increase timeout value

--- publish_release.sh
+++ publish_release.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-./gradlew publish --no-daemon --no-parallel
-./gradlew closeAndReleaseRepository
+# We manually run assemble & dokka before uploadArchives. If we only run uploadArchives,
+# the assemble and dokka tasks are run interleaved on each module, which can cause
+# connection timeouts to Sonatype (since we need to wait for assemble+dokka to finish).
+# By front-loading the assemble+dokka tasks, the upload is much quicker.
+./gradlew assembleRelease dokkaHtml publish closeAndReleaseRepository --no-daemon --no-parallel
 
