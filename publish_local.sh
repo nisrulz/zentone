11c29e5d3d2557de46419f2fa2161b182b9db788
Nishant Srivastava <nisrulz@gmail.com>
2022-07-13 01:06:36 +0200

    Make upload to Sonatype less flaky

--- publish_local.sh
+++ publish_local.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-./gradlew publishToMavenLocal
+# We manually run assemble & dokka before uploadArchives. If we only run uploadArchives,
+# the assemble and dokka tasks are run interleaved on each module, which can cause
+# connection timeouts to Sonatype (since we need to wait for assemble+dokka to finish).
+# By front-loading the assemble+dokka tasks, the upload is much quicker.
+./gradlew assembleRelease dokkaHtml publishToMavenLocal --no-daemon --no-parallel
