e20ebd0ffcfafe2c73bddb5c7598ed8c58433e3c
Nishant Srivastava <nisrulz@gmail.com>
2023-12-05 20:42:34 +0100

    cleanup in publish script and gradle properties file

--- publish_release.sh
+++ publish_release.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-# We manually run assemble & dokka before uploadArchives. If we only run uploadArchives,
+# We manually run assemble & dokka before publishAllPublicationsToMavenCentral. If we only
+# run publishAllPublicationsToMavenCentral,
 # the assemble and dokka tasks are run interleaved on each module, which can cause
 # connection timeouts to Sonatype (since we need to wait for assemble+dokka to finish).
 # By front-loading the assemble+dokka tasks, the upload is much quicker.
-./gradlew assembleRelease dokkaHtml publish closeAndReleaseRepository --no-daemon --no-parallel
-
+./gradlew assembleRelease dokkaHtml publishAllPublicationsToMavenCentral --no-configuration-cache -PRELEASE_SIGNING_ENABLED=true
