3631789e7169208ca20f225f808ce2f0a1e88805
Nishant Srivastava <nisrulz@gmail.com>
2025-05-03 17:00:20 +0200

    updated MainActivity

--- app/src/main/java/com/github/nisrulz/samplezentone/MainActivity.kt
+++ app/src/main/java/com/github/nisrulz/samplezentone/MainActivity.kt
@@ -18,42 +18,32 @@ package com.github.nisrulz.samplezentone
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.github.nisrulz.samplezentone.ui.screen.main.MainScreen
 import com.github.nisrulz.samplezentone.ui.theme.ZenToneProjectTheme
-import com.github.nisrulz.zentone.ZenTone
 
 class MainActivity : ComponentActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-    setContent {
-      ZenToneProjectTheme {
-        // A surface container using the 'background' color from the theme
-        Surface(
-          modifier = Modifier.fillMaxSize(),
-          color = MaterialTheme.colorScheme.background,
-        ) {
-          MainScreen()
+        enableEdgeToEdge()
+
+        setContent {
+            ZenToneProjectTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background,
+                ) {
+                    // ViewModel
+                    MainScreen()
+                }
+            }
         }
-      }
     }
-  }
 }
 
-@Composable
-fun MainScreen(modifier: Modifier = Modifier) {
-  val zenTone = ZenTone()
-}
 
-@Preview(showBackground = true)
-@Composable
-private fun Preview() {
-  ZenToneProjectTheme {
-    MainScreen()
-  }
-}
