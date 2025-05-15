1f969ab1d7f3dd27ba3309b1e0ed31a2b749b250
Nishant Srivastava <nisrulz@gmail.com>
2025-05-15 19:40:57 +0200

    added theme related dimens and shapes

---
+++ app/src/main/java/com/github/nisrulz/samplezentone/ui/theme/AppDimensions.kt
@@ -0,0 +1,18 @@
+package com.github.nisrulz.samplezentone.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+internal object AppDimensions {
+    val dimen0 = 0.dp
+    val dimen2 = 2.dp
+    val dimen4 = 4.dp
+    val dimen8 = 8.dp
+    val dimen12 = 12.dp
+    val dimen16 = 16.dp
+    val dimen24 = 24.dp
+    val dimen32 = 32.dp
+    val dimen40 = 40.dp
+    val dimen48 = 48.dp
+    val dimen56 = 56.dp
+    val dimen64 = 64.dp
+}
---
+++ app/src/main/java/com/github/nisrulz/samplezentone/ui/theme/AppTheme.kt
@@ -0,0 +1,7 @@
+package com.github.nisrulz.samplezentone.ui.theme
+
+internal object AppTheme {
+    val dimension = AppDimensions
+    val typography = AppTypography
+    val shapes = AppShapes
+}
