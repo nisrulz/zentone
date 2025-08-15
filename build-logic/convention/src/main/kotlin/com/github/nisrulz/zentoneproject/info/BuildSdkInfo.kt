795cce056d22a634df75af0bac0bdc2422210890
Nishant Srivastava <nisrulz@gmail.com>
2025-08-15 16:06:13 +0200

    update compile sdk to 36

--- build-logic/convention/src/main/kotlin/com/github/nisrulz/zentoneproject/info/BuildSdkInfo.kt
+++ build-logic/convention/src/main/kotlin/com/github/nisrulz/zentoneproject/info/BuildSdkInfo.kt
@@ -1,7 +1,7 @@
 package com.github.nisrulz.zentoneproject.info
 
 internal object BuildSdkInfo {
-    const val COMPILE_SDK_VERSION = 35
+    const val COMPILE_SDK_VERSION = 36
     const val MIN_SDK_VERSION = 21
     const val TARGET_SDK_VERSION = 35
     const val JVM_TARGET = 17
