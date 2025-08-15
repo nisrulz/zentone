25aa4225bf20a8a6e5e69a6f3e473d7fbc23992d
Nishant Srivastava <nisrulz@gmail.com>
2025-08-15 16:06:22 +0200

    update min sdk to 23

--- build-logic/convention/src/main/kotlin/com/github/nisrulz/zentoneproject/info/BuildSdkInfo.kt
+++ build-logic/convention/src/main/kotlin/com/github/nisrulz/zentoneproject/info/BuildSdkInfo.kt
@@ -2,7 +2,7 @@ package com.github.nisrulz.zentoneproject.info
 
 internal object BuildSdkInfo {
     const val COMPILE_SDK_VERSION = 36
-    const val MIN_SDK_VERSION = 21
+    const val MIN_SDK_VERSION = 23
     const val TARGET_SDK_VERSION = 35
     const val JVM_TARGET = 17
 }
